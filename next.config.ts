b215d8136701c981dbbc46960cddfdac30252e8b
moba1 <moba2@protonmail.com>
2025-05-07 13:57:39 +0900

    FAQ と連絡先に関しては next.config.ts で設定できるようにした

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  env: {
+    faqLink: "https://github.com/izzet-mtg/random-commander-jp/wiki",
+    contactLink: "https://github.com/izzet-mtg/random-commander-jp/issues",
+  }
 };
 
 export default nextConfig;
