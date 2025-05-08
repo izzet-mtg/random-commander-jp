c216f12fc4cfa38296a17d3dec929dea1894fcbb
moba1 <moba2@protonmail.com>
2025-05-08 23:52:29 +0900

    ライトモードのときのヘッダーカラーを調整してロゴを見易くした

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -1,5 +1,5 @@
 const Header = () =>  (
-  <header className="flex flex-col flex-wrap w-full dark:bg-gray-800 bg-gray-300 p-4 gap-2">
+  <header className="flex flex-col flex-wrap w-full dark:bg-gray-800 bg-gray-600 p-4 gap-2">
     <div>
       {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
       <a href={`${process.env.NEXT_PUBLIC_BASE_PATH}/`} className="inline-block">
