30bdc3f16324be11a75b418f76e1f8c9f817dab2
moba1 <moba2@protonmail.com>
2025-05-10 19:53:00 +0900

    カード情報とイラストの間にギャップを明示的に置くようにした

--- src/component/card/Face.tsx
+++ src/component/card/Face.tsx
@@ -4,7 +4,7 @@
 import { PropsWithChildren } from "react";
 
 const Face = ({ children }: PropsWithChildren) => (
-  <div className="grid md:grid-cols-2 grid-cols-1 max-w-[1280px]">
+  <div className="grid md:grid-cols-2 grid-cols-1 max-w-[1280px] gap-8">
     {children}
   </div>
 );
