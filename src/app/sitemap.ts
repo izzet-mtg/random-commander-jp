67526d7d1f48a9d231d5b26c0a554d9234e6571e
moba1 <moba2@protonmail.com>
2025-08-10 16:14:58 +0900

    サイトマップ追加

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: process.env.NEXT_PUBLIC_SITE_URL,
+      lastModified: '2025-08-10',
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/about-us`,
+      lastModified: '2025-08-10',
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/faq`,
+      url: 'https://acme.com/faq',
+      lastModified: '2025-08-10',
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/privacy-policy`,
+      lastModified: '2025-08-10',
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap`,
+      lastModified: '2025-08-10',
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+  ]
+}
