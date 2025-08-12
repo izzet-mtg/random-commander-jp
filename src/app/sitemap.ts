ad7a8e8ff81a5bb3c0a5705d2dd726a5e8a68a5a
moba1 <moba2@protonmail.com>
2025-08-12 19:37:52 +0900

    sitemap の日付は常に new Date するようにした

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,34 +3,35 @@ import { MetadataRoute } from 'next';
 export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const today = new Date();
   return [
     {
       url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
-      lastModified: '2025-08-10',
+      lastModified: today,
       changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/faq`,
-      lastModified: '2025-08-10',
+      lastModified: today,
       changeFrequency: 'daily',
       priority: 0.5,
     },
     {
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/about-us`,
-      lastModified: '2025-08-10',
+      lastModified: today,
       changeFrequency: 'daily',
       priority: 0.5,
     },
     {
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/privacy-policy`,
-      lastModified: '2025-08-10',
+      lastModified: today,
       changeFrequency: 'daily',
       priority: 0.5,
     },
     {
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/human-sitemap`,
-      lastModified: '2025-08-10',
+      lastModified: today,
       changeFrequency: 'daily',
       priority: 0.5,
     },
